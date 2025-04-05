595bb3fe44a03f59d369dbbd8fb951683003958d
Tailan <tailansanttos02@gmail.com>
2025-04-05 12:14:36 -0300

    feat: modificação no dto User

--- src/main/java/com/tailan/investimentos/model/dtos/UserDto.java
+++ src/main/java/com/tailan/investimentos/model/dtos/UserDto.java
@@ -1,4 +1,17 @@
 package com.tailan.investimentos.model.dtos;
 
-public record UserDto(String username, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserDto(
+
+        @NotBlank(message = "Username é obrigatorio.")
+        String username,
+
+        @NotBlank(message = "O e-mail é obrigatório.")
+        @Email(message = "O e-mail deve ser válido.")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatorio.")
+        String password) {
 }
