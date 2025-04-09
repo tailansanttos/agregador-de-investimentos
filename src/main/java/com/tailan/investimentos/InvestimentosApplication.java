3b8f9ae22c9d4301f98d9cfee5c3c6c8bbcaec2b
Tailan <tailansanttos02@gmail.com>
2025-04-09 14:47:50 -0300

    feat: anotação para utilizar o openFeign,e tambem o swagger.

--- src/main/java/com/tailan/investimentos/InvestimentosApplication.java
+++ src/main/java/com/tailan/investimentos/InvestimentosApplication.java
@@ -1,9 +1,14 @@
 package com.tailan.investimentos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Agregador de Investimentos", version = "1.0", description = "API para usuários investirem."))
 public class InvestimentosApplication {
 
 	public static void main(String[] args) {
