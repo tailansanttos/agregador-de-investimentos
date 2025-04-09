a8e5d58800d5c5b4a9518802128af0d847468c2a
Tailan <tailansanttos02@gmail.com>
2025-04-09 14:42:58 -0300

    feat: Atualização na entidade user.

--- src/main/java/com/tailan/investimentos/model/domain/User.java
+++ src/main/java/com/tailan/investimentos/model/domain/User.java
@@ -1,9 +1,12 @@
 package com.tailan.investimentos.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_users")
@@ -15,6 +18,11 @@ public class User {
     private Long id;
     private String username;
     private String password;
+
+    /// UM USUARIO TEM VARIAS CONTAS
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER) //MAPEIA QUE JA FOI FEITA NO ACCOUNT, E O NOME DO ATRIBUTO
+    @JsonManagedReference
+    private List<Account> accounts;
 
     @Column(unique = true)
     private String email;
@@ -49,6 +57,14 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
     }
 
     public User() {
