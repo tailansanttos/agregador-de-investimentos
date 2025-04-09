e68f990ee759ce402b963e558deae156b7395c6f
Tailan <tailansanttos02@gmail.com>
2025-04-09 14:47:05 -0300

    feat: criado para tratar as exception personalizadas da aplicação.

--- src/main/java/com/tailan/investimentos/exceptions/ExceptionHandleGlobal.java
+++ src/main/java/com/tailan/investimentos/exceptions/ExceptionHandleGlobal.java
@@ -1,12 +1,15 @@
 package com.tailan.investimentos.exceptions;
 
 import com.tailan.investimentos.model.dtos.ErrorResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+
 
 @RestControllerAdvice
 public class ExceptionHandleGlobal {
@@ -20,6 +23,6 @@ public class ExceptionHandleGlobal {
     @ExceptionHandler(UserEmailExistsException.class)
     public ResponseEntity<ErrorResponse> userEmailExistsException(UserEmailExistsException ex) {
         ErrorResponse newError = new ErrorResponse(LocalDateTime.now(), HttpStatus.NO_CONTENT.value(), "Email já cadastrado.", ex.getMessage());
-        return new ResponseEntity<>(newError, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(newError, HttpStatus.BAD_REQUEST);
     }
 }
